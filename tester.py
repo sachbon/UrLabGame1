928d210e9357de38bc009b03e07a82c8fd9e4631
rballant <ballantromain@gmail.com>
2018-11-13 08:49:43 +0100

    update sprite (niew biome)

--- tester.py
+++ tester.py
@@ -3,5 +3,6 @@ import testing_tools as tt
 
 board = me.create_board(6,8)
 me.set_platform(board,[3,2],3)
-
+me.set_player_spawn(board,[1,2],1)
+me.set_player_spawn(board,[4,5],2)
 tt.printer(board)
