6512ab0d42f9bd7ae3e9e85cbefd43126b20997f
fgrimau <florent.grimau@ulb.be>
2018-11-13 19:26:57 +0100

    Updates for comments on code

--- constant.py
+++ constant.py
@@ -1,7 +1,14 @@
+"""
+    Author : fgrimau @jurofloem
+    
+"""
+
 import pygame
 from pygame.locals import *
 
+#Initialization of the graphic library
 pygame.init()
 
+#Loading images
 Background01 = pygame.image.load("sprites/background01.png").convert_alpha()
 platform00   = pygame.image.load("sprites/platform00.png").convert_alpha()
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import time
-from gameManager import gameManager as gM
-
 """
     Author: Olfg, fgrimau
     Date: 12/11/18
     Project: Skeleton for the main.py file for Turn Based Platformer (TBP)
 """
 
-#Implementing Board
+import time
+from gameManager import gameManager as gM
+
+#Implementing Board as a matrix wich contains the position of each platforms
 board = [[[False, 0],[False, 0],[False, 0],[False, 0],[True , 0],[True , 0],[False, 0],[False, 0]],
          [[False, 0],[False, 0],[False, 0],[False, 0],[False, 0],[False, 0],[False, 0],[False, 0]],
          [[False, 0],[False, 0],[True , 0],[True , 0],[True , 0],[True , 0],[False, 0],[False, 0]],
@@ -15,8 +15,10 @@ board = [[[False, 0],[False, 0],[False, 0],[False, 0],[True , 0],[True , 0],[Fal
          [[False, 0],[False, 0],[False, 0],[False, 0],[False, 0],[False, 0],[False, 0],[False, 0]],
          [[False, 0],[True , 0],[True , 0],[True , 0],[True , 0],[True , 0],[True , 0],[False, 0]]]
 
-screen = gM((256, 192), "L.A.G.")
+#Setting up the gameManager class, wich manipulate the screen, the time and the sounds
+gameManagager = gM((256, 192), "L.A.G.")
 
+#importing every images loaded in constants
 from constant import *
 
 #Setting up multiple variables
@@ -31,16 +33,14 @@ running = True #While true, it will make the game update, if it steps to false, 
 currentStep = 0 #The current step of the game (0, player 1 inputs his next movements, 1, player 2 inputs his next movement, 3 the game shows the movements of the players)
 
 while running:
-    timeElapsed = time.time() - startTime
-    startTime = time.time()
 
-    screen.blit(Background01, (0, 0))
+    gameManagager.blit(Background01, (0, 0)) #Blits the background on the screen
     for y in range(len(board)):
         for x in range(len(board[y])):
             if board[y][x][0]:
-                screen.blit(platform00, (x*32, y*32))
+                gameManagager.blit(platform00, (x*32, y*32)) #Blits a platform image if the x/y position on the matrix is true
 
-    screen.flip()
+    gameManagager.flip() #Updates the screen
 
     for event in pygame.event.get():
         if event.type == QUIT:
