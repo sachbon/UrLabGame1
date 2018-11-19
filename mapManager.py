9c22a1ee398cef4524585be913e76a6777b7e221
fgrimau <florent.grimau@ulb.be>
2018-11-19 14:50:54 +0100

    updating python scripts

---
+++ mapManager.py
@@ -0,0 +1,16 @@
+from maps import maps
+
+class mapManager(object):
+
+    def __init__(self):
+        self.size = None
+        self.mapPath = None
+
+    def loadMap(self, path):
+        
+
+    def draw(self, screen):
+        #DRAWING THE MAP
+
+    def update(self, timeELapsed):
+        #UPDATES THE MAP
